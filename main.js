66f8f518699864af338709762579d34ab59b4d6f
naranovarltan <ar.naranov@gmail.com>
2020-02-06 23:35:35 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const users = {
+  userId1: {
+    "01-02-2020": true,
+    "02-02-2020": true
+  },
+  userId2: {
+    "02-02-2020": true
+  },
+  userId3: {
+    "03-02-2020": true
+  },
+  userId4: {
+    "04-02-2020": true
+  }
+};
